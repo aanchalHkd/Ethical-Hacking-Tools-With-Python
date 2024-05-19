11e9233334b19119cb718ea254597f658d96366f
Aanchal Singh <53784480+aanchalHkd@users.noreply.github.com>
2024-05-19 10:50:45 +0530

    Update Network Scanner Using Scapy.py

--- Network Scanner Using Scapy.py
+++ Network Scanner Using Scapy.py
@@ -1,4 +1,5 @@
-import scapy.all as scapy # type: ignore
+import scapy.all as scapy
+import optparse
 
 
 # make sure the code is in the same network as the range that you want to scan
@@ -6,6 +7,16 @@ import scapy.all as scapy # type: ignore
 #     scapy.arping(ip)
 
 # scan("10.3.5.8/24") # provide the IP range/network that we want to scan
+
+def get_argument():
+    parser = optparse.OptionParser() #everything that starts with capital latter in python is a cloass
+
+    parser.add_option("-ip","--iprange",dest="ip",help="Ip Address or range to scan")
+    (option, argument)= parser.parse_args()
+    if not option.ip:
+        parser.error("[-] Please specify an IP Range. Use --help for more details")
+    
+    return option
 
 
 def scan(ip):
@@ -19,10 +30,23 @@ def scan(ip):
     #print(answered.summary()) - shows which clients received the ARP packet and sends their MAC
 
     #answered_list contains the 2 parts. 1- request, 2 - response(IP and MAC address of the receiver)
-    print("IP\t\t\tMAC Address\n--------------------------")
+    #print("IP\t\t\tMAC Address\n--------------------------")
+    clients_list =[]
+
     for element in answered_list:
-        print(element[1].psrc+"\t\t\t"+element[1].hwsrc) # prints the receiver IP from 2nd part of the answered_list
-        
+        client_dict= {"ip":element[1].psrc, "mac":element[1].hwsrc}
+        clients_list.append(client_dict)
+        #print(element[1].psrc+"\t\t\t"+element[1].hwsrc) # prints the receiver IP from 2nd part of the answered_list
         #print(element[1].hwsrc) #prints the MAC address of the receiver
         print("-------------------------------------------------")
+    return clients_list
 
+def print_result(result_list):
+    print("IP\t\t\tMAC Address\n--------------------------")
+    for client in result_list:
+        print(client["ip"]+"\t\t\t"+client["mac"])
+
+Scan_ip = get_argument()
+
+scan_result=scan(Scan_ip.ip)
+print_result(scan_result)
